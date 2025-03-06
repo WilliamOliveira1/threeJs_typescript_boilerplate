952913f733d025235ea2cc97e38c3fb33cd01830
WilliamOliveira1 <1704639@aluno.univesp.br>
2025-03-06 00:16:34 -0300

    add gui methods summary

--- src/guiHelper.ts
+++ src/guiHelper.ts
@@ -10,6 +10,14 @@ export class GuiHelper {
         this.three = THREE;
     }
 
+    /**
+     * Add a gui for a 3d mesh object
+     * @param element A 3d mesh object
+     * @param elementName The name of the element
+     * @param axis The axis to add to the gui
+     * @param min The minimum value
+     * @param max The maximum value 
+     */
     addMeshGuiElementFolder(element: THREE.Mesh, elementName: string, axis: (keyof THREE.Euler)[], min: number, max: number): void {
         const elementFolder = this.gui?.addFolder(elementName);
         axis.forEach(elementAxis => {
@@ -18,6 +26,14 @@ export class GuiHelper {
         elementFolder?.open();
     }
 
+    /**
+     * Add a gui for a PerspectiveCamera
+     * @param element a perspective camera
+     * @param elementName the name of the element
+     * @param axis The axis to add to the gui
+     * @param min The minimum value
+     * @param max The maximum value
+     */
     addMeshGuiVectorFolder(element: THREE.PerspectiveCamera, elementName: string, axis: (keyof THREE.Vector3)[], min: number, max: number): void {
         const elementFolder = this.gui?.addFolder(elementName);
         axis.forEach(elementAxis => {
